94d922d3176accac3f1af331f2c629b3fc46ec24
Juan Jose Diaz Diaz <juanjosdazisla@gmail.com>
2023-10-04 11:04:57 +0200

    EJERCICIOS

---
+++ src/Boletin1/Ej7.java
@@ -0,0 +1,22 @@
+package Boletin1;
+
+import java.util.Scanner;
+
+public class Ej7 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int num;
+        int suma = 0;
+        do {
+            System.out.println("introduce varios numeros");
+            num = teclado.nextInt();
+        }
+
+        do {
+
+
+
+        } while ();
+        System.out.println("esta sería la media de los siguientes numeros: " + media);
+    }
+}
