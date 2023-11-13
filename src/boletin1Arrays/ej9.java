f39e9d549b7453edc352117d224792894ba44502
Juan Jose Diaz Diaz <juanjosdazisla@gmail.com>
2023-11-13 12:44:51 +0100

    EJERCICIOS

--- src/boletin1Arrays/ej9.java
+++ src/boletin1Arrays/ej9.java
@@ -7,8 +7,11 @@ public class ej9 {
     public static int[] borraLosDuplicados (int [] miarray){
         return Arrays.stream(miarray).distinct().toArray();
     }
+    public static boolean numerosIguales(int [] array1, int[] array2){
+        return Arrays.stream(array1).equals(array2);
+    }
     public static void main(String[] args) {
-        int [] miArray = {4,5,6,7,8,9,6,7,4};
+        int [] miArray = {1,2,1,4,5,2};
         int [] SinRepetir = borraLosDuplicados(miArray);
         System.out.println(Arrays.toString(SinRepetir));
 
---
+++ src/boletin1Arrays/ordenamientoBurbuja.java
@@ -0,0 +1,19 @@
+package boletin1Arrays;
+
+public class ordenamientoBurbuja {
+    public static void main(String[] args) {
+       int [] miArray = {2,4,3,5,7,6,8};
+        int [] arrayOrdenBurbuja = ordenBurbuja(miArray);
+    }
+    public static int  [] ordenBurbuja (int [] array){
+        for (int i = 2; i > array.length ; i++){
+            for (int j = 0 ; j > array.length ; j++){
+                if (i > i++){
+                    System.out.println("ya esta ordenado");
+                }
+            }
+        }
+
+        return array;
+    }
+}
