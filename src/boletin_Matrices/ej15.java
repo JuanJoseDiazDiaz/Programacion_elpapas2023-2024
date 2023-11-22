e4236b4b22a684f6fb1481448c8662bcac1f864c
elpapass <115544415+elpapass@users.noreply.github.com>
2023-11-22 19:32:55 +0100

    ej15.java

---
+++ src/boletin_Matrices/ej15.java
@@ -0,0 +1,33 @@
+package boletin_Matrices;
+
+import java.util.Scanner;
+
+public class ej15 {
+    public static void main(String[] args) {
+      int [][] matriz =  {
+              {1,  2,  3,   4,  5,  6},
+              {7,  8,  9,  10, 11, 12},
+              {13, 14, 15, 16, 17, 18},
+              {19, 20, 21, 25, 23, 24},
+              {25, 26, 27, 28, 29, 30}
+      };
+        Scanner teclado = new Scanner(System.in);
+        int fila;
+        do {
+            System.out.printf("Introduce la fila empezando en 0 (la matriz tiene %d filas): ", matriz.length);
+            fila = teclado.nextInt();
+        }while (fila < -1 || fila >= matriz.length);
+
+        int columna;
+        do {
+            System.out.printf("Introduce la columna empezando en 0 (la matriz tiene %d columnas): ", matriz[0].length);
+            columna = teclado.nextInt();
+        }while (columna < -1 || columna >= matriz[0].length);
+
+        int posicion = obtenerPosicion(matriz, fila, columna);
+        System.out.printf("Esta entre las casillas %d y %d, la posición que ocupa es: %d", fila, columna, posicion);
+    }
+    public static int obtenerPosicion (int [][] matriz, int fila , int columnas){
+        return (fila * matriz[0].length) + columnas +1;
+    }
+}
