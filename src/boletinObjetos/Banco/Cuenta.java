d1391721de005f20807cd4ba9f4953bfde8b27e6
Juan Jose Diaz Diaz <juanjosdazisla@gmail.com>
2023-12-04 12:45:44 +0100

    EJERCICIOS

---
+++ src/boletinObjetos/Banco/Cuenta.java
@@ -0,0 +1,40 @@
+package boletinObjetos.Banco;
+
+import java.util.Scanner;
+
+public class Cuenta {
+
+    public static double reintegroDinero(double saldoInicial){
+        Scanner teclado = new Scanner(System.in);
+        double resultado = 0;
+        System.out.println("Su saldo actual es de: "+ saldoInicial);
+        System.out.println("Introduce la cantidad a retirar: ");
+        double cantidadRetirada = teclado.nextDouble();
+
+        resultado = saldoInicial - cantidadRetirada;
+        System.out.println("su saldo actual es de: "+ resultado);
+        return resultado;
+    }
+    public static double ingresoDinero(double saldoInicial){
+        Scanner teclado = new Scanner(System.in);
+        double saldoFinal = 0;
+        System.out.println("Su saldo actual es de: "+ saldoInicial);
+        System.out.println("Introduce la cantidad a ingresar: ");
+        double cantidadIngresada = teclado.nextDouble();
+
+        saldoFinal = saldoInicial + cantidadIngresada;
+        System.out.println("su saldo actual es de: "+ saldoFinal);
+        return saldoFinal;
+    }
+
+    public static void comprobacionOperaciones (double saldoInicial, int numeroReintegros, int numeroIntegros){
+        System.out.println("SU SALDO ACTUAL ES DE :" + saldoInicial);
+        System.out.println("la operacion de reintegros ha sido usada: "+ numeroReintegros);
+        System.out.println("la operacion de reintegros ha sido usada: "+ numeroIntegros);
+    }
+
+
+
+
+
+}
---
+++ src/boletinObjetos/Banco/Principal.java
@@ -0,0 +1,48 @@
+package boletinObjetos.Banco;
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int numeroIntegros = 0;
+        int numeroReintegros = 0;
+        System.out.println("introduce tu saldo inicial: ");
+        double saldoIncial = teclado.nextDouble();
+        imprimirMenu();
+
+        int opciones = teclado.nextInt();
+       do {
+           switch (opciones){
+               case 1:
+                   Cuenta.reintegroDinero(saldoIncial);
+                   System.out.println("Se a realizado el reintegro");
+                   numeroReintegros++;
+                   break;
+
+               case 2:
+                   Cuenta.ingresoDinero(saldoIncial);
+                   System.out.println("Se a realizado el propio ingreso");
+                   numeroIntegros++;
+                   break;
+
+               case 3:
+                   Cuenta.comprobacionOperaciones(saldoIncial, numeroReintegros, numeroIntegros);
+                   System.out.println("Se a realizado la operacion");
+                   break;
+               case 4:
+                   System.out.println("ADIOS");
+                   break;
+           }
+       }while (opciones >= 4);
+
+    }
+    public static void imprimirMenu(){
+        System.out.println("QUE OPERACION QUIERES HACER: ");
+        System.out.println("INGRESA CUALQUIER OPCION (1-4): ");
+            System.out.println("1. Hacer un reintegro, se pedirá la cantidad a retirar.");
+            System.out.println("2. Hacer un ingreso, se pedirá la cantidad a ingresar.");
+            System.out.println("3. Consultar el saldo y el número de reintegros e ingresos realizados.");
+            System.out.println("4. Finalizar las operaciones. Debe confirmar si realmente desea salir e informar del saldo al final de todas las operaciones.  ");
+    }
+}
