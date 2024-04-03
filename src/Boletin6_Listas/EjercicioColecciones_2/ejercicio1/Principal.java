2d3c2b65b1902b1641d0ed3d4bac3688d9b936ef
Juan Jose Diaz Diaz <juanjosdazisla@gmail.com>
2024-04-03 12:01:29 +0200

    EJERCICIOS

--- src/Boletin6_Listas/EjercicioColecciones_2/ejercicio1/Principal.java
+++ src/Boletin6_Listas/EjercicioColecciones_2/ejercicio1/Principal.java
@@ -1,6 +1,6 @@
 package Boletin6_Listas.EjercicioColecciones_2.ejercicio1;
 
-import boletinRepasoExamen.Avion.EjerciciosRepaso.ExamenColecciones.ejercicio1.NetPlease;
+
 
 public class Principal {
 
