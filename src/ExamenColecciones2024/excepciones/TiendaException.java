a33e039cf098d73e6fe176b87403c5024a836951
Juan Jose Diaz Diaz <juanjosdazisla@gmail.com>
2024-04-04 11:52:56 +0200

    EJERCICIOS

---
+++ src/ExamenColecciones2024/excepciones/TiendaException.java
@@ -0,0 +1,7 @@
+package ExamenColecciones2024.excepciones;
+
+public class TiendaException extends Exception{
+    public TiendaException(String message) {
+        super(message);
+    }
+}
---
+++ src/ExamenColecciones2024/modelo/Categoria.java
@@ -0,0 +1,9 @@
+package ExamenColecciones2024.modelo;
+
+import java.util.Set;
+
+public class Categoria {
+    private String nombre;
+    private Set<Producto> productos;
+
+}
