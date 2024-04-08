68fef4ce6b74182ef55e73914827c7f648ddde94
Juan Jose Diaz Diaz <juanjosdazisla@gmail.com>
2024-04-08 12:44:35 +0200

    EJERCICIOS

--- src/BoletinFicheros/Ej2/Ej2.java
+++ src/BoletinFicheros/Ej2/Ej2.java
@@ -1,18 +1,30 @@
 package BoletinFicheros.Ej2;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class Ej2 {
     public static void main(String[] args){
-        File archivoNuevo = new File(".\\src\\BoletinFicheros\\FicheroNuevo.txt");
+        /* File archivoNuevo = new File(".\\src\\BoletinFicheros\\FicheroNuevo.txt");
         try(BufferedReader br = new BufferedReader(new FileReader(archivoNuevo))) {
-            while (br.readLine() != null){
-
+            String linea;
+            StringBuilder frase = new StringBuilder();
+            while ((linea = br.readLine()) != null){
+                frase.append(linea);
             }
-
+            System.out.println("El contenido del fichero es: "+ frase);
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
+
+         */
+        Path path = Paths.get(".\\src\\BoletinFicheros\\FicheroNuevo.txt");
+        try {
+            Stream<String> stream = Files.lines(path);
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
