9e4d32b2705a5a0f7cc09ab428cd317ab52f9f80
Juan Jose Diaz Diaz <juanjosdazisla@gmail.com>
2024-04-24 12:45:17 +0200

    EJERCICIOS

--- src/BoletinFicheros7_3/Ej2.java
+++ src/BoletinFicheros7_3/Ej2.java
@@ -23,11 +23,47 @@ public class Ej2 {
             Element titulo = (Element) e.getElementsByTagName("title").item(0);
             System.out.println("Este es el nombre del titulo de la pagWeb: " + titulo.getTextContent());
 
-            NodeList div = (NodeList) documento.getElementsByTagName("div").item(0);
+            NodeList div = documento.getElementsByTagName("div");
             System.out.println("Numero de div de la pagWeb: " + div.getLength());
 
-            NodeList divValores = (NodeList) documento.getElementsByTagName("id").item(0);
-            System.out.println("Numero de div de la pagWeb: " + div.getLength());
+            NodeList divValores = documento.getElementsByTagName("div");
+            int contDivValores = 0;
+            for (int i = 0; i < divValores.getLength(); i++){
+                Element element = (Element) divValores.item(i);
+                if (!element.getAttribute("id").isBlank()){
+                    contDivValores++;
+                }
+            }
+            System.out.println("Numero de div con valores de la pagWeb: " + contDivValores);
+
+            NodeList imgValores = documento.getElementsByTagName("img");
+            for (int i = 0; i < imgValores.getLength(); i++){
+                Element element = (Element) imgValores.item(i);
+                if (!element.getAttribute("alt").isBlank()){
+                    System.out.println("El texto es el siguiente: " + element.getAttribute("alt"));
+                }
+            }
+
+            NodeList todosLosDiv = documento.getElementsByTagName("div");
+            for (int i = 0; i < todosLosDiv.getLength(); i++){
+                Element element = (Element) todosLosDiv.item(i);
+                if (element.getAttribute("class").equals("noticia")){
+                   String titular = element.getElementsByTagName("h2").item(0).getTextContent();
+                    System.out.println("Este es el titular de la noticia: "+ titular);
+                    String textoAlT =((Element) element.getElementsByTagName("img").item(0)).getAttribute("alt");
+                    System.out.println("Este es el texto alternativo de la imagen: "+ textoAlT);
+
+
+                }
+
+            }
+
+
+
+
+
+
+
 
 
 
