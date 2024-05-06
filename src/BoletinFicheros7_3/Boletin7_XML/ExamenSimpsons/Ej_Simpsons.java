35e8be8f08cd6da909aa299b8c9d2d2c9353ce31
Juan Jose Diaz Diaz <juanjosdazisla@gmail.com>
2024-05-06 13:53:41 +0200

    EJERCICIOS

--- src/BoletinFicheros7_3/Boletin7_XML/ExamenSimpsons/Ej_Simpsons.java
+++ src/BoletinFicheros7_3/Boletin7_XML/ExamenSimpsons/Ej_Simpsons.java
@@ -8,13 +8,17 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 import java.io.IOException;
 import java.util.stream.Stream;
 
 public class Ej_Simpsons {
     public static void main(String[] args) {
-        try {
 
+        try {
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db = dbf.newDocumentBuilder();
             Document documento = db.parse(".\\src\\BoletinFicheros7_3\\Boletin7_XML\\ExamenSimpsons\\Simpsons.html");
@@ -34,21 +38,65 @@ public class Ej_Simpsons {
                 }
             }
 
-            for (int i = 0; i < capitulos.getLength();i++){
+            System.out.println("\nEstas son las frases: ");
+            for (int i = 0; i < capitulos.getLength(); i++) {
                 Element capitulo = (Element) capitulos.item(i);
                 String sinopsis = capitulo.getElementsByTagName("sinopsis").item(0).getTextContent();
-                if (sinopsis.length() >= 4){
-                    int recuentoLetra = Integer.parseInt(sinopsis.substring(sinopsis.length()));
-                    if (recuentoLetra > 30){
-                        System.out.println(sinopsis);
-                    }
-
+                if (sinopsis.length() > 156) {
+                    System.out.println(capitulo.getElementsByTagName("sinopsis").item(0).getTextContent() + " \n");
                 }
 
             }
 
+            boolean siEsta = false;
+            String nombreBart = null;
+            String nombreLisa = null;
+            for (int i = 0; i < capitulos.getLength(); i++) {
+                Element capitulo = (Element) capitulos.item(i);
+                if (capitulo.getElementsByTagName("sinopsis").item(0).getTextContent().contains("Bart") || capitulo.getElementsByTagName("sinopsis").item(0).getTextContent().contains("Lisa")) {
+                    nombreBart = capitulo.getElementsByTagName("sinopsis").item(0).getTextContent().replaceAll("Bart", "*Bart*");
+                    nombreLisa = capitulo.getElementsByTagName("sinopsis").item(0).getTextContent().replaceAll("Lisa", "*Lisa*");
+                    siEsta = true;
 
-        } catch (ParserConfigurationException | IOException | SAXException e) {
+                }
+            }
+            Element newElement = documento.createElement("capitulo");
+            newElement.setAttribute("temporada", String.valueOf(capitulos.getLength() + 1));
+            Element newSinopsis = documento.createElement("sinopsis");
+            newSinopsis.setTextContent("Esto es una prueba donde aparece " + nombreBart + " y se acaba");
+            newSinopsis.setTextContent("Esto es una prueba donde aparece " + nombreLisa + " y se acaba");
+
+
+            File f = new File(".\\src\\BoletinFicheros7_3\\Boletin7_XML\\ExamenSimpsons\\Simpsons2.html");
+
+            // 2º Creamos una nueva instancia del transformador a través de la fábrica de
+            // transformadores.
+
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+
+            // 3º Establecemos algunas opciones de salida, como por ejemplo, la codificación
+            // de salida.
+
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+
+            // 4º Creamos el StreamResult, que intermediará entre el transformador y el
+            // archivo de destino.
+
+            StreamResult result = new StreamResult(f);
+
+            // 5º Creamos el DOMSource, que intermediará entre el transformador y el árbol
+            // DOM.
+
+            DOMSource source = new DOMSource(documento);
+
+            // 6º Realizamos la transformación.
+
+            transformer.transform(source, result);
+
+
+        } catch (ParserConfigurationException | IOException | SAXException | TransformerException e) {
             throw new RuntimeException(e);
         }
 
