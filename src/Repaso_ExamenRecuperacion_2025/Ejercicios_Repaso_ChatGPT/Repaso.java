f8b64ba67f52dabdd6fb2f1d7f3470db3ecddfb5
elpapass <115544415+elpapass@users.noreply.github.com>
2025-01-23 20:08:30 +0100

    -- Ejercicios de Respaso Examen Recuperacion Programacion 2025

---
+++ src/Repaso_ExamenRecuperacion_2025/Ejercicios_Repaso_ChatGPT/Repaso.java
@@ -0,0 +1,167 @@
+package Repaso_ExamenRecuperacion_2025.Ejercicios_Repaso_ChatGPT;
+
+import java.util.*;
+
+public class Repaso {
+    public static void main(String[] args) {
+//        ArrayList<String> lista = new ArrayList<>();
+//        lista.add("Juan");
+//        lista.add("Ana");
+//        lista.add("Sofia");
+//        lista.add("Luis");
+//
+//
+//
+//        // Imprime todos los datos de la lista
+//        System.out.println("Lista de nombres:");
+//        for (int i = 0; i < lista.size(); i++) {
+//
+//            System.out.println(lista.get(i));
+//        }
+//        System.out.println("\n");
+//        // Elimina a luis de la lista
+//        lista.remove("Luis");
+//
+//        // Imprime todos los datos de la lista, sin Luis
+//        System.out.println("Lista eliminada de luis");
+//        for (int i = 0; i < lista.size(); i++) {
+//
+//            System.out.println(lista.get(i));
+//        }
+//
+//        HashSet<Integer> hashSetNumero = new HashSet<>();
+//        hashSetNumero.add(1);
+//        hashSetNumero.add(2);
+//        hashSetNumero.add(3);
+//        hashSetNumero.add(4);
+//        hashSetNumero.add(5);
+//        hashSetNumero.add(2);
+//        hashSetNumero.add(4);
+//
+//        if (hashSetNumero.contains(3)) {
+//            System.out.println(hashSetNumero);
+//        }else{
+//            System.out.println("No se puede imprimir la lista");
+//        }
+
+//        HashMap<String, Float> listaCompra = new HashMap<>();
+//        listaCompra.put("Manzana", 1.2f);
+//        listaCompra.put("Platano", 0.8f);
+//        listaCompra.put("Naranja", 1.5f);
+//
+//        System.out.println("El precio de la naranja es de: "+listaCompra.get("Naranja"));
+//
+//        listaCompra.remove("Platano");
+//        System.out.println("Producto borrado!");
+//
+//        System.out.println(listaCompra.entrySet());
+
+//        ArrayList<Integer> listaNum = new ArrayList<>();
+//        listaNum.add(1);
+//        listaNum.add(9);
+//        listaNum.add(4);
+//        listaNum.add(8);
+//        listaNum.add(6);
+//        listaNum.add(10);
+//        listaNum.add(3);
+//        listaNum.add(5);
+//        listaNum.add(7);
+//
+//        System.out.println(listaNum.stream().sorted((a, b) -> a - b).toList());
+
+//        ArrayList<Integer> listaNumero = new ArrayList<>();
+//        listaNumero.add(10);
+//        listaNumero.add(15);
+//        listaNumero.add(20);
+//        listaNumero.add(25);
+//        listaNumero.add(30);
+//        List<Integer> listaNumero2 = listaNumero.stream().filter(num -> num > 20).toList();
+//        System.out.println(listaNumero2);
+
+//        ArrayList<String> listaColores = new ArrayList<>();
+//        listaColores.add("rojo");
+//        listaColores.add("azul");
+//        listaColores.add("rojo");
+//        listaColores.add("verde");
+//        listaColores.add("azul");
+//        listaColores.add("azul");
+//
+//        HashMap<String, Integer> frecuencia = new HashMap<>();
+//
+//        // Recorrer la lista de colores
+//        for (String color : listaColores) {
+//            // Si el color ya está en el HashMap, incrementar el contador
+//            if (frecuencia.containsKey(color)) {
+//                frecuencia.put(color, frecuencia.get(color) + 1);
+//            } else {
+//                // Si no está, agregarlo con un contador inicial de 1
+//                frecuencia.put(color, 1);
+//            }
+//        }
+//
+//        // Imprimir los resultados
+//        System.out.println("Frecuencia de colores:");
+//        for (String color : frecuencia.keySet()) {
+//            System.out.println(color + ": " + frecuencia.get(color));
+//        }
+
+//        Stack<Integer> stack = new Stack<>();
+//        stack.push(10);
+//        stack.push(20);
+//        stack.push(30);
+//        stack.push(40);
+//        stack.push(50);
+//        System.out.println("Stack Inicial: "+stack);
+//
+//        stack.pop();
+//        System.out.println("Stack Remodelado: "+stack);
+
+//        Queue<String> queueList = new LinkedList<>();
+//        queueList.add("Pedro");
+//        queueList.add("María");
+//        queueList.add("José");
+//
+//        System.out.println(queueList.peek());
+//        queueList.poll();
+//        System.out.println(queueList);
+
+//        HashSet<Integer> listaNum1 = new HashSet<>();
+//        listaNum1.add(1);
+//        listaNum1.add(2);
+//        listaNum1.add(3);
+//        listaNum1.add(4);
+//        listaNum1.add(5);
+//
+//        HashSet<Integer> listaNum2 = new HashSet<>();
+//        listaNum2.add(4);
+//        listaNum2.add(5);
+//        listaNum2.add(6);
+//        listaNum2.add(7);
+//        listaNum2.add(8);
+//
+//        System.out.println("Union -> ");
+//        HashSet<Integer> union = new HashSet<>(listaNum1);
+//        union.addAll(listaNum2);
+//        System.out.println("Esta es la union de ambos: "+ union);
+//
+//        System.out.println("Intersection -> ");
+//        HashSet<Integer> insercion = new HashSet<>(listaNum1);
+//        insercion.retainAll(listaNum2);
+//        System.out.println("Esta es la insercion de ambos: "+insercion);
+
+//        LinkedList<String> abecedario = new LinkedList<>();
+//        abecedario.add("A");
+//        abecedario.add("B");
+//        abecedario.add("C");
+//        abecedario.add("D");
+//
+//        ListIterator<String> iterator = abecedario.listIterator();
+//        while (iterator.hasNext()) {
+//            System.out.println(iterator.next());
+//        }
+//        System.out.println("\n");
+//        while (iterator.hasPrevious()) {
+//            System.out.println(iterator.previous());
+//        }
+    }
+}
