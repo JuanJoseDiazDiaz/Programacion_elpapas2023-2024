232a8546281705a031a934baebf0f523ef0b7fd9
elpapass <115544415+elpapass@users.noreply.github.com>
2025-03-10 21:02:13 +0100

    -- Ejercicios de Respaso Examen Recuperacion Programacion - Ficheros 2025

---
+++ src/RepasoDeFicheros2025/Ej_Avanzado/Ej7/Ej7_Ex.java
@@ -0,0 +1,9 @@
+package RepasoDeFicheros2025.Ej_Avanzado.Ej7;
+
+import java.io.File;
+
+public class Ej7_Ex {
+    public static void main(String[] args) {
+        File contactosControl = new File("");
+    }
+}
